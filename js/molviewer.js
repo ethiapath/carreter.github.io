f804905347a74af225ec22808d99eda165f42f5c
Willow Carretero Chavez <sandiegobutterflies@gmail.com>
2022-12-21 17:01:13 -0500

    Fix broken display

--- js/molviewer.js
+++ js/molviewer.js
@@ -8,7 +8,7 @@ $( document ).ready(() => {
         [ "#F7A8B8", "396-494" ],
     ], "Transmembrane 1a52" );
     stage = new NGL.Stage('mol-container', {backgroundColor: "#ebebeb"})
-    stage.loadFile('rcsb://1a52').then((o) => {
+    stage.loadFile('./1a52.pdb').then((o) => {
         console.log(o)
         o.addRepresentation( "cartoon", { color: schemeId } );
         stage.autoView()
